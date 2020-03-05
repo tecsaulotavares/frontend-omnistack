247948d253c6bc90d16aeeeb67275f0a26a45ebe
Saulo Sousa <saulo.sousa@venosan.com.br>
2020-03-05 17:13:04 -0300

    alterações de api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
+import api from "../../services/api";
 
 import logo from "../../assets/logo.svg";
 import "./styles.css";
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleSubmit = () => {};
   render() {
     return (
       <div id="main-container">
-        <form action="">
+        <form onSubmit={this.handleSubmit} action="">
           <img src={logo} alt="" />
           <input placeholder="Criar um box" />
           <button type="submit">Criar</button>
