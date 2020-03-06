d3de96c984496019884fd9ab673e6ac0dc3243bd
Saulo Sousa <saulo.sousa@venosan.com.br>
2020-03-06 07:54:57 -0300

    atualizando aplicação no git

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,17 +5,37 @@ import logo from "../../assets/logo.svg";
 import "./styles.css";
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  // }
+  state = {
+    newBox :''
   }
-  handleSubmit = () => {};
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const response = await api.post('boxes',{
+      title: this.state.newBox
+    })
+    console.log(response.data)
+  };
+  handleInputChange = (e) => {
+    this.setState({
+      newBox : e.target.value
+    })
+  }
   render() {
     return (
       <div id="main-container">
         <form onSubmit={this.handleSubmit} action="">
           <img src={logo} alt="" />
-          <input placeholder="Criar um box" />
+          <input 
+            placeholder="Criar um box" 
+            value = {this.state.newBox}
+            onChange={this.handleInputChange}
+            />
           <button type="submit">Criar</button>
         </form>
       </div>
