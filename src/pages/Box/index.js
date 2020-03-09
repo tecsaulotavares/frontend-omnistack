7705c3889810dfc0e035526bd93b5f98756ebf84
Saulo Sousa <saulo.sousa@venosan.com.br>
2020-03-09 10:09:41 -0300

    Aplicação frontend

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import api from "../../services/api";
-import { distanceInWords } from "date-fns";
+import { formatDistanceToNow } from "date-fns";
 import pt from "date-fns/locale/pt";
 import logo from "../../assets/logo.svg";
 import Dropzone from "react-dropzone";
+import socket from "socket.io-client";
 
 import { MdInsertDriveFile } from "react-icons/md";
 
@@ -14,11 +15,35 @@ class Box extends Component {
     box: {}
   };
   async componentDidMount() {
+    this.subscribeToNewFiles();
     const box = this.props.match.params.id;
     const response = await api.get(`boxes/${box}`);
 
     this.setState({ box: response.data });
   }
+
+  subscribeToNewFiles = () => {
+    const box = this.props.match.params.id;
+    const io = socket("http://localhost:3333");
+
+    io.emit("connectRoom", box);
+
+    io.on("file", data => {
+      this.setState({
+        box: { ...this.state.box, files: [data, ...this.state.box.files] }
+      });
+    });
+  };
+
+  handleUpload = files => {
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+      data.append("file", file);
+
+      api.post(`boxes/${box}/files`, data);
+    });
+  };
   render() {
     return (
       <div id="box-container">
@@ -26,7 +51,7 @@ class Box extends Component {
           <img src={logo} alt="" />
           <h1>{this.state.box.title}</h1>
         </header>
-        {/* <Dropzone>
+        <Dropzone onDropAccepted={this.handleUpload}>
           {({ getRootProps, getInputProps }) => (
             <div className="upload" {...getRootProps()}>
               <input {...getInputProps()} />
@@ -34,18 +59,18 @@ class Box extends Component {
               <p>Arraste arquivos ou clique aqui</p>
             </div>
           )}
-        </Dropzone> */}
+        </Dropzone>
         <ul>
           {this.state.box.files &&
             this.state.box.files.map(file => (
-              <li>
+              <li key={file.id}>
                 <a className="fileInfo" href={file.url} target="_blank">
                   <MdInsertDriveFile size={24} color="#A5CFFF" />
                   <strong>{file.title}</strong>
                 </a>
                 <span>
                   há{" "}
-                  {distanceInWords(file.createdAt, new Date(), {
+                  {formatDistanceToNow(new Date(file.createdAt), {
                     locale: pt
                   })}
                 </span>
